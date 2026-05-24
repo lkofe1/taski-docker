40b8e65f94c0533feb5415f21a03f0c2d2cbd038
lkofe1 <denisburan5@gmail.com>
2026-05-24 09:48:39 +0400

    ystal1112

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,18 @@ from api import views
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.http import HttpResponse
+
+
+def home_view(request):
+    return HttpResponse("Taski API is running", status=200)
+
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
